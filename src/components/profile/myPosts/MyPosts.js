59b3e4fa57ee00627e5cadc92c4d37bf8fa591c0
shnol <shnolik@gmail.com>
2020-07-17 02:20:09 +0300

    day4

--- src/components/profile/myPosts/MyPosts.js
+++ src/components/profile/myPosts/MyPosts.js
@@ -3,7 +3,10 @@ import cls from './MyPosts.module.css';
 import Post from "./post/Post";
 
 const myPosts = (props) => {
+
     let postslist = props.posts.map( post => <Post message={post.text} like={post.like} id={post.id}/>)
+    const addpost = () =>  {alert('kyky blyz')}
+    let newPostEl = React.createRef()
     return (
         <div>
             <div className={cls.header}>
@@ -11,7 +14,7 @@ const myPosts = (props) => {
             </div>
             <div className={cls.item}>
                 <textarea></textarea>
-                <button>Add post</button>
+                <button onClick={addpost}>Add post</button>
                 {postslist}
             </div>
         </div>
